b86ed41319f534c67769d525647dc0c6c4ac99dd
itsxavs <javitoo.lol@gmail.com>
2024-06-15 04:04:18 +0200

    m

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:
-    "http://next-day-api-env.eba-sfg4eywe.eu-north-1.elasticbeanstalk.com/",
+  apiUrl: "http://13.60.99.195:3000/",
 };
